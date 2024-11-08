bf3495e3b2065ba4b2e1941b5cdc5501be242967
Baguettou <68337546+Baguettou@users.noreply.github.com>
2024-11-08 13:40:24 -0500

    Fixed images & changed webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,62 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-    entry: './src/main.js', //  Apunta a main.js en la raíz.
+    entry: './src/main.js',
     output: {
-      filename: 'main.js',
-      path: path.resolve(__dirname, 'dist'),
+        publicPath: '/', // Ruta pública para los recursos (importante para el despliegue)
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.bundle.js',
+        clean: true
     },
-    module:{
-        rules:[
-            {test:/\.css$/, use:['style-loader', 'css-loader']},
-
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                // Solo usar MiniCssExtractPlugin.loader y css-loader
+                use: [MiniCssExtractPlugin.loader, "css-loader"], 
+                exclude: [/node_modules\/leaflet/,/node_modules\/bootstrap/], // Excluir el CSS de leaflet y bootstrap para que no sean procesados como módulos
+            },
             {
                 test: /\.svg$/,
                 use: [
                     {
                         loader: 'svg-url-loader',
                         options: {
-                            limit: 10000, // Convierte SVGs menores a 10KB a data URLs.  Ajusta según tus necesidades
-                            // encoding: 'base64'
+                            limit: 10000,
                         }
                     }
-                    
                 ]
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'imgs/[name][ext]'
+                }
             },
         ],
     },
-    
-    plugins:[
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "styles.css", 
+        }),
         new HtmlWebpackPlugin({
-            template: './src/index.html' // También index.html en la raíz.
-        })
+            template: './src/index.html',
+            filename: './index.html',
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: "src/imgs", to: "imgs" },
+                { from: "src/components", to: "components" }
+            ],
+        }),
     ],
-    devServer:{
-        static: './', // Sirve archivos estáticos desde la raíz.
+    devServer: {
+        static: './dist',
+        port: 8081, 
     },
     mode: 'development'
-  };
+};
